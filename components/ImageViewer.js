5493db6f8443b723284273434e47244b3bdd7cb2
Shreejai Raj <shreejairaj@gmail.com>
2024-08-19 11:20:28 +1000

    pressable buttons

--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -2,7 +2,7 @@ import { StyleSheet, Image } from "react-native";
 
 export default function ImageViewer({ placeholderImageSource }) {
   return (
-    <Image source={ placeholderImageSource }  />
+    <Image source={ placeholderImageSource } style={styles.image} />
   )
 } 
 
